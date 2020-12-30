87edb224e350d1e6803f444fed4a6efa807e2d69
Mecser Levente <56694893+mecserlevente@users.noreply.github.com>
2020-12-30 19:12:54 +0100

    Add files via upload

---
+++ breaking the records/src/Solution.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the breakingRecords function below.
+    static int[] breakingRecords(int[] scores) {
+        int min = scores[0];
+        int max = scores[0];
+        int mindb = 0;
+        int maxdb = 0;
+        for (int e : scores) {
+            if (e > max) {
+                maxdb++;
+                max = e;
+            }
+            if (e < min) {
+                mindb++;
+                min = e;
+            }
+        }
+     int[] eredmeny=new int[2];
+        eredmeny[0]=maxdb;
+        eredmeny[1]=mindb;
+        return eredmeny;
+    }
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] scores = new int[n];
+
+        String[] scoresItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int scoresItem = Integer.parseInt(scoresItems[i]);
+            scores[i] = scoresItem;
+        }
+
+        int[] result = breakingRecords(scores);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
